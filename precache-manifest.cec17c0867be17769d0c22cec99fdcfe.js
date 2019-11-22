f6a65652b1124c8276f86a4c43fdebc792bb3483
Dylan Salay <dylansalay@Dylans-MacBook-Pro.local>
2019-11-21 17:56:14 -0800

    Updates

---
+++ precache-manifest.cec17c0867be17769d0c22cec99fdcfe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2e7c17eca75f7f33e1239281c190660",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "6f749bcc43db163bfa8e",
+    "url": "/Monsters-Rolodex/static/css/main.958e9da5.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/Monsters-Rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "6f749bcc43db163bfa8e",
+    "url": "/Monsters-Rolodex/static/js/main.74e4ec66.chunk.js"
+  },
+  {
+    "revision": "11e2dea3cac39d985a42",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.3ef3660b.js"
+  }
+]);
